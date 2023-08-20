fc78f2625fc99972f059c21253a0fca048fc31f0
Jeff Martson <jeffmartson@gmail.com>
2023-08-20 14:59:30 +0300

    ver2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+
 def calculate_ticket_price(age):
     if age < 18:
         return 0
@@ -6,19 +9,47 @@ def calculate_ticket_price(age):
     else:
         return 1390
 
-def main():
-    num_tickets = int(input("Введите количество билетов: "))
+def calculate_total_price():
+    num_tickets = int(num_tickets_entry.get())
     total_price = 0
 
     for _ in range(num_tickets):
-        age = int(input("Введите возраст посетителя: "))
+        age = int(age_entries[_].get())
         ticket_price = calculate_ticket_price(age)
         total_price += ticket_price
 
     if num_tickets > 3:
-        total_price *= 0.9  # Применяем 10% скидку при заказе более 3 билетов
+        total_price *= 0.9  # Apply 10% discount for more than 3 tickets
 
-    print(f"Сумма к оплате: {total_price:.2f} руб.")
+    messagebox.showinfo("Total Cost", f"Сумма к оплате: {total_price:.2f} руб.")
 
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.title("Conference Ticket Price Calculator")
+
+num_tickets_label = tk.Label(root, text="Количество билетов:")
+num_tickets_label.pack()
+
+num_tickets_entry = tk.Entry(root)
+num_tickets_entry.pack()
+
+age_labels = []
+age_entries = []
+
+def add_age_fields():
+    num_tickets = int(num_tickets_entry.get())
+
+    for i in range(num_tickets):
+        age_labels.append(tk.Label(root, text=f"Возраст посетителя {i + 1}:"))
+        age_labels[i].pack()
+
+        age_entries.append(tk.Entry(root))
+        age_entries[i].pack()
+
+    calculate_button.pack()
+
+add_button = tk.Button(root, text="Добавить", command=add_age_fields)
+add_button.pack()
+
+calculate_button = tk.Button(root, text="Рассчитать стоимость", command=calculate_total_price)
+
+root.mainloop()
